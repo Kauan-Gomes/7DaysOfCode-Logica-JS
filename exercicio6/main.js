7256c7e7965398868b2dcff79462fcdbea974eaf
Kauan pc giovanna <Kauang13@gmail.com>
2023-06-06 21:22:43 -0300

    Exercicio 6 resolvido

--- exercicio6/main.js
+++ exercicio6/main.js
@@ -190,60 +190,51 @@ function adicionarItem (){
         doces.push(item)
     }
 
-
     telaPrincipal()
     
 }
 
-
+ //função para remover item
 function removeritem (botao,elementos){
 
     botao.addEventListener("click", evento =>{
         let valor = elementos.textContent
         
+        //split remove o valor que está no parametro, se estiver sem nada remove os espaços, e transforma em arrays
         valor = valor.split('X')
+        //por causa do split ficou um array de dois elementos[elemento,""], usei o pop para remover esse ultimo 
         valor.pop()
         valorNovo = valor[0]
         
-
+        //a função indexOF() busca no array o valor passado em parametro, retornando a posição do mesmo, caso não seja encontrado a função retorna -1
         var indexFrutas = frutas.indexOf(valorNovo) 
         var indexCongelados = congelados.indexOf(valorNovo)
         var indexDoces = doces.indexOf(valorNovo)
         var indexLatnicios = lat.indexOf(valorNovo)
 
-        const filtroFruta = frutas.find(element => element = valorNovo)
-        const filtroCongelados = congelados.find(element => element = valorNovo)
-        const filtroDoces = doces.find(element => element = valorNovo)
-        const filtroLat = lat.find(element => element = valorNovo)
-        
-        console.log(frutas)
-        console.log(congelados)
-        console.log(doces)
-        console.log(lat)
 
-        if(filtroFruta != -1){
+        //validação para poder remover os itens de compras nos arrays, caso o indexOF encontre o elemento, o valor será diferente de -1,
+        if(indexFrutas != -1){
+            //splice remove elementos do array, sendo o primeiro valor a posição a ser removida, e o segundo valor a quantidade de elementos
             frutas.splice(indexFrutas, 1)
-            console.log(frutas)
         }
-        if(filtroCongelados != -1){
+        if(indexCongelados != -1){
             congelados.splice(indexCongelados, 1)
         }
-        if(filtroDoces != -1){
+        if(indexDoces != -1){
             doces.splice(indexDoces, 1)
             
         }
-        if(filtroLat != -1){
+        if(indexLatnicios != -1){
             lat.splice(indexLatnicios, 1)
             
         }
 
-        elementos.nodeParent.removeChild(elementos)
+
+        //Para remover os elementos do DOM, o parentNode acessa o Pai, e com removeChild é possivel remover o filho, que no caso é o proprío elemento
+        elementos.parentNode.removeChild(elementos)
 
     })
-
-
-
-
 }
 
 
