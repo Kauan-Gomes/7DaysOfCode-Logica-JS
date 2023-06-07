9893b88adfe732ed1fec4bc51a54650a8c990160
Kauan Gomes <Kauang13@gmail.com>
2023-06-07 17:26:20 -0300

    exercicio 7 resolvido

--- exercicio7/main.js
+++ exercicio7/main.js
@@ -4,22 +4,47 @@ console.log(input)
 
 console.log(valores)
 
-const conta1 = []
+let conta1 = []
 
 valores.forEach(element => {
     element.addEventListener('click', evento => {
         const conta = evento.target.textContent
-
         conta1.push(conta)
+        const Operacao = conta1.join('')
 
-        
-        //console.log(conta1)
-
+        input.value = Operacao
        
+        if(conta === 'Apagar'){
+            reset(conta1)
+        }
+
+        if (conta === '=') {
+
+            const operacao2 = Operacao.substring(0, Operacao.length - 1)
+            
+
+            const resultado = eval(operacao2)
+
+            conta1.splice(0, conta1.length)
         
+            conta1.push()
+            input.value = resultado
+            
+            conta1.push(resultado)
+        }
+
+        
+
     })
 });
 
 
+function reset(array) {
+
+    input.value = ''
+    array.splice(0,array.length)
+
+
+}
 
 
